a4c8ef178e022d72872585c50d7e55b34b1d3935
Matt Horne <abyss616@yahoo.com>
2023-12-10 17:15:13 -0500

    fixing arch

--- roles/arch/files/script.sh
+++ roles/arch/files/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd $HOME
-# xdg-user-dirs-update
+xdg-user-dirs-update
 # dconf load /com/ < tilix.dconf
 # cd git && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 # cd $HOME/.config/i3blocks/scripts/bandwidth2
 # make
-sudo smbpasswd -a mattarch
+# sudo smbpasswd -a matt
